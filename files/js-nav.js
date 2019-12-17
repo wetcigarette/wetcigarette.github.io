a875ace294bf655da7ed8d0606555ce4b69474c6
anthony.shackell <anthony.shackell@workday.com>
2019-12-17 15:06:13 -0800

    Base site

---
+++ files/js-nav.js
@@ -0,0 +1,9 @@
+/* Add class="updated" or class="new" to the link tag. */
+
+document.write('<h2>Navigation</h2>');
+document.write('<ul>');
+document.write('<li><a href="index.html">Home</a></li>');
+document.write('<li><a href="gallery.htm">Pix</a></li>');
+document.write('<li><a href="shows.htm">Shows</a></li>');
+document.write('<li><a href="contact.htm">Contact</a></li>');
+document.write('</ul>');
