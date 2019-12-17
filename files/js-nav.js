bdee0b79b7ef7f40e1cf1240684187253aad7bf1
anthony.shackell <anthony.shackell@workday.com>
2019-12-17 15:45:46 -0800

    update link in nav

--- files/js-nav.js
+++ files/js-nav.js
@@ -2,7 +2,7 @@
 
 document.write('<h2>Navigation</h2>');
 document.write('<ul>');
-document.write('<li><a href="index.html">Home</a></li>');
+document.write('<li><a href="index.htm">Home</a></li>');
 document.write('<li><a href="gallery.htm">Pix</a></li>');
 document.write('<li><a href="shows.htm">Shows</a></li>');
 document.write('<li><a href="contact.htm">Contact</a></li>');
