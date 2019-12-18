924e160e93bbe255bafe2585ea8b98167441b731
Anthony Shackell <anthony@kwesi.local>
2019-12-17 22:18:51 -0800

    Refactor base site

--- files/js-footer.js
+++ files/js-footer.js
@@ -1,2 +1,2 @@
 document.write('Wet Cigarette - Copyright 2020 your mom industries, fuck you<br>');
-document.write('All images, trademarks, and copyrights are property of their respective owners.<br><br>');
+document.write('All images, trademarks, and copyrights are property<br><br>');
