d3337f64c28d9d01f18999136b066bae9795f49e
anthony.shackell <anthony.shackell@workday.com>
2020-05-07 14:14:26 -0700

    add link to bandcamp

--- files/js-nav.js
+++ files/js-nav.js
@@ -4,6 +4,7 @@ document.write('<h2>Navigation</h2>');
 document.write('<ul>');
 document.write('<li><a href="index.htm">Home</a></li>');
 document.write('<li><a href="gallery.htm">Pix</a></li>');
+document.write('<li><a href="https://w3tcigarette.bandcamp.com">Music</a></li>');
 document.write('<li><a href="shows.htm">Shows</a></li>');
 document.write('<li><a href="contact.htm">Contact</a></li>');
 document.write('</ul>');
